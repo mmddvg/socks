6c280dc7921025b9dada9297905ec04abe8c8b01
Mohammad Davatgar <md2002.110@gmail.com>
2025-06-28 19:49:19 +0330

    feat : add filtering

--- main.go
+++ main.go
@@ -16,8 +16,28 @@ import (
 	"unsafe"
 )
 
+var blocked map[string]bool = map[string]bool{
+	"127.0.0.1": true,
+	// "96.7.128.175":  true, // example.com ips
+	// "23.192.228.80": true,
+	// "23.192.228.84": true,
+	// "23.215.0.138":  true,
+	// "23.215.0.136":  true,
+}
+
 func handleConnection(cfd C.int, inp *C.req) {
 	defer C.close(cfd)
+
+	ipStr := byteToIp(inp)
+
+	log.Printf("target ip : %s", ipStr)
+
+	if blocked[ipStr] {
+		log.Printf("ip blocked: %s\n", ipStr)
+		response := C.make_connect_res(91, inp.dest_port, inp.dest_ip)
+		C.send(cfd, unsafe.Pointer(&response), C.size_t(unsafe.Sizeof(response)), 0)
+		return
+	}
 
 	if inp.cd == 1 {
 		fmt.Println("connect")
@@ -36,11 +56,24 @@ func handleConnection(cfd C.int, inp *C.req) {
 
 		C.bi_proxy(cfd, destfd)
 		C.close(destfd)
+
 	} else if inp.cd == 2 {
 		fmt.Println("bind")
 	} else {
 		fmt.Println("invalid cd")
 	}
+}
+
+func byteToIp(inp *C.req) string {
+	ipNetOrder := C.ntohl(inp.dest_ip)
+	ipBytes := []byte{
+		byte(ipNetOrder >> 24),
+		byte(ipNetOrder >> 16),
+		byte(ipNetOrder >> 8),
+		byte(ipNetOrder),
+	}
+	ipStr := fmt.Sprintf("%d.%d.%d.%d", ipBytes[0], ipBytes[1], ipBytes[2], ipBytes[3])
+	return ipStr
 }
 
 func main() {
